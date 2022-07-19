ffe82241a84d0d86787659c781f71019f243a4bd
Hamas Ahmed <hamasahmed341@gmail.com>
2022-07-19 20:00:57 +0500

    Code Cleanup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import {HomeSection1} from "./pages/HomeSection1";
-import {HomeSection2} from "./pages/HomeSection2";
-import {HomeSection3} from "./pages/HomeSection3";
+import Index from "./pages/Index";
 
 function App() {
     return (
         <div className="App">
-            <HomeSection1/>
-            <HomeSection2/>
-            <HomeSection3/>
+            <Index/>
         </div>
     );
 }
